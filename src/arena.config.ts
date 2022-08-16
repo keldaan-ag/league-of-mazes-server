57fe59e15ddc9f2371aa79877cc296841975dc2e
Keldaan <arnaudgregoire92@gmail.com>
2022-08-16 14:18:10 +0200

    remove express routes

--- src/arena.config.ts
+++ src/arena.config.ts
@@ -1,6 +1,5 @@
 import Arena from "@colyseus/arena";
 import { monitor } from "@colyseus/monitor";
-import path from "path"
 
 /**
  * Import your Room files
@@ -17,23 +16,6 @@ export default Arena({
         gameServer.define('my_room', MyRoom);
 
     },
-
-    initializeExpress: (app) => {
-        /**
-         * Bind your custom express routes here:
-         */
-        app.get("/", (req, res) => {
-            res.sendFile(path.join(__dirname, '../public/index.html'));
-        });
-
-        /**
-         * Bind @colyseus/monitor
-         * It is recommended to protect this route with a password.
-         * Read more: https://docs.colyseus.io/tools/monitor/
-         */
-        app.use("/colyseus", monitor());
-    },
-
 
     beforeListen: () => {
         /**
