a64882136e2ac320690791cdd59e4ce457dc00ef
Keldaan <arnaudgregoire92@gmail.com>
2022-08-16 12:39:04 +0200

    colyseus server

---
+++ src/arena.config.ts
@@ -0,0 +1,43 @@
+import Arena from "@colyseus/arena";
+import { monitor } from "@colyseus/monitor";
+import path from "path"
+
+/**
+ * Import your Room files
+ */
+import { MyRoom } from "./rooms/MyRoom";
+
+export default Arena({
+    getId: () => "Your Colyseus App",
+
+    initializeGameServer: (gameServer) => {
+        /**
+         * Define your room handlers:
+         */
+        gameServer.define('my_room', MyRoom);
+
+    },
+
+    initializeExpress: (app) => {
+        /**
+         * Bind your custom express routes here:
+         */
+        app.get("/", (req, res) => {
+            res.sendFile(path.join(__dirname, '../public/index.html'));
+        });
+
+        /**
+         * Bind @colyseus/monitor
+         * It is recommended to protect this route with a password.
+         * Read more: https://docs.colyseus.io/tools/monitor/
+         */
+        app.use("/colyseus", monitor());
+    },
+
+
+    beforeListen: () => {
+        /**
+         * Before before gameServer.listen() is called.
+         */
+    }
+});
