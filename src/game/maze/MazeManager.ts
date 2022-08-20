5cf120c0651f5ec819c5a15c36b3ccb05a6d1f85
Keldaan <arnaudgregoire92@gmail.com>
2022-08-20 23:24:06 +0200

    fix

--- src/game/maze/MazeManager.ts
+++ src/game/maze/MazeManager.ts
@@ -24,12 +24,14 @@ export class MazeManager{
     generateCells(){
         for (let xi = 0; xi < this.template!.width; xi++) {
             for (let yi = 0; yi < this.template!.height; yi++) {
+                const isEntry = this.template!.entry.x === xi && this.template!.entry.y === yi
+                const isExit = this.template!.exit.x === xi && this.template!.exit.y === yi
                 this.template!.data[this.template!.height * xi + yi] = {
                     x: xi,
                     y: yi,
-                    isEntry: this.template!.entry.x === xi && this.template!.entry.y === yi,
-                    isExit: this.template!.exit.x === xi && this.template!.exit.y === yi,
-                    isWall: this.template!.entry.x !== xi && this.template!.entry.y !== yi && Math.random() > 0.9,
+                    isEntry: isEntry,
+                    isExit: isExit,
+                    isWall: !isEntry && !isExit && Math.random() > 0.9,
                     isPath: false
                 }
             }
